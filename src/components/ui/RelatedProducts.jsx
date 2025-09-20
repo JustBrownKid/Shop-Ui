10fdca243b68fdb3ac786168cb52037ad713beab
JustBronwKid <larakid8080@gmail.com>
2025-09-20 14:01:48 +0630

    Ui

--- src/components/ui/RelatedProducts.jsx
+++ src/components/ui/RelatedProducts.jsx
@@ -1,8 +1,22 @@
-function RelatedProduct() {
-    return (
-        <div className="related-products">
-            <h2 className="text-2xl font-bold mb-6">Related Products</h2>
-        </div>
-    );
-}
-export  default RelatedProduct;
+import React from 'react';
+import ItemCard from './ItemCard';
+
+const items = new Array(20).fill(null);
+
+const RelatedProducts = () => {
+  return (
+    <ul className="flex overflow-x-auto list-none p-0 m-0">
+      {items.map((_, index) => (
+          <li key={index} className="flex-none w-52">
+          <ItemCard
+            title="Test Title For item"
+            price={`150000 MMK`}
+            src={`https://placehold.co/800?text=This is ${index}&font=roboto`}
+          />
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default RelatedProducts;
